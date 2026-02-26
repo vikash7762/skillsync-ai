668feccf634905ba689c93669d55a377f084542e
vikash7762 <vikashkumar9504168773@gmail.com>
2026-02-26 17:40:08 +0530

    Updated db.py for local + deployment compatibility

--- database/db.py
+++ database/db.py
@@ -2,9 +2,13 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+# Load .env locally (won't affect Streamlit Cloud)
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    raise ValueError("MONGO_URI is not set in environment variables.")
 
 client = MongoClient(MONGO_URI)
 db = client["skillsync_db"]
